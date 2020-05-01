4ee0af09d59458edf4502f8c9f8ad5423e3a1808
Babatunde Lamidi <babatunde@mevron.com>
2020-05-01 14:04:28 +0100

    ⚡️ looks good for now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MainComponent } from './components/main/main.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SubscribeComponent } from './subscribe/subscribe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MainComponent
+    MainComponent,
+    SubscribeComponent
   ],
   imports: [
     BrowserModule,
